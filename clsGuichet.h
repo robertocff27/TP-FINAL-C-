34a92c3a15207e513f5d18316ffe745f1807b192
robertocff27 <44532089+robertocff27@users.noreply.github.com>
2019-08-13 17:13:10 -0400

    Create clsGuichet.h

---
+++ clsGuichet.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <string>
+
+using namespace std;
+class clsGuichet
+{
+private:
+	string Numero;
+	string Compagnie;
+	string Adresse;
+	//clsListeCompte Comptes;
+	string Statut;
+	float Solde;
+public:
+	clsGuichet();
+
+};
